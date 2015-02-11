baacbb46224a091b1b1febd170df532ff5f4af2d
acreeser <alexcreeser@gmail.com>
2015-02-11 17:06:48 -0600

    fixed bugs, added refresh and auto refresh and deletes

--- root/chartbugz.js
+++ root/chartbugz.js
@@ -1,5 +1,5 @@
 (function () {
-    var chartbugz = window.chartbugz || {};
+    var chartbugz = window.chartbugz || {}, AUTO_INTERVAL_TIMEOUT = 60000, autoIntervalID;
 
     if (localStorage == null) {
         $("#pie").html("Your browser does not support local storage");
@@ -8,6 +8,7 @@
     chartbugz.initialize = function () {
         chartbugz.filterMap = JSON.parse(localStorage.getItem("chartbugz.filterMap")) || { "default": "default.rss" };
         chartbugz.defaultFilterNickname = localStorage.getItem("chartbugz.defaultFilterNickname") || "default";
+		chartbugz.setAutoRefresh( localStorage.getItem("chartbugz.autoRefresh") || true, false );
         chartbugz.currentFilterNickname = chartbugz.defaultFilterNickname;
         chartbugz.wireEvents();
         chartbugz.refreshFilterList();
@@ -23,44 +24,79 @@
             chartbugz.refreshFilterList();
             chartbugz.saveFilterList();
         });
+		$(".refresh-list input[type=checkbox]")[0].checked = chartbugz.autoRefresh;
+		$(".refresh-list input[type=checkbox]").change(function() {
+			chartbugz.setAutoRefresh($(this)[0].checked, true);
+		});
+		$(".refresh-list button").click(function() {
+			chartbugz.loadData();
+		});
     };
-
+	
+	chartbugz.setAutoRefresh = function(doAutoRefresh, doSave) {
+		chartbugz.autoRefresh = doAutoRefresh;
+		if (doSave) {
+			localStorage.setItem("chartbugz.autoRefresh", chartbugz.autoRefresh);
+		}
+		if (doAutoRefresh && autoIntervalID == null) {
+			autoIntervalID = setInterval(chartbugz.loadData, AUTO_INTERVAL_TIMEOUT);
+		} else if (!doAutoRefresh && autoIntervalID != null) {
+			clearInterval(autoIntervalID);
+		}
+	};
+	
     chartbugz.saveFilterList = function () {
         localStorage.setItem("chartbugz.filterMap", JSON.stringify(chartbugz.filterMap));
         localStorage.setItem("chartbugz.defaultFilterNickname", chartbugz.currentFilterNickname);
     };
-
+	
+	function curriedDelete(nickname){
+		return function() {
+			chartbugz.filterMap[nickname] = undefined;
+			chartbugz.saveFilterList();
+			$(this).parent().empty();
+		};
+	}
     chartbugz.refreshFilterList = function () {
-        $("#currentList").empty();
-        for (var nickname in chartbugz.filterMap) {
-            var newLink = document.createElement("a");
+		var nickname, newLink, delLink, newDiv;
+        $(".current-list").empty();
+        for (nickname in chartbugz.filterMap) {
+            newLink = document.createElement("a");
             newLink.href = "javascript:void(0);";
             newLink.text = nickname;
             $(newLink).click(function () {
                 chartbugz.currentFilterNickname = this.text;
                 chartbugz.loadData();
-            })
-            var newDiv = document.createElement("div");
+            });
+            delLink = document.createElement("a");
+            delLink.href = "javascript:void(0);";
+            delLink.text = "[x]";
+			$(delLink).css({"margin-left": 15});
+            $(delLink).click(curriedDelete(nickname));
+            newDiv = document.createElement("div");
             $(newDiv).append(newLink);
+			$(newDiv).append(delLink);
             $(".current-list").append(newDiv);
         }
         
     };
 
     chartbugz.loadData = function () {
-        var data = [];
-        var subUrl = chartbugz.filterMap[chartbugz.currentFilterNickname];
-        jQuery.getFeed({
+        var data = [], subUrl = chartbugz.filterMap[chartbugz.currentFilterNickname];
+        $.getFeed({
             url: (subUrl == "default.rss") ? "http://localhost:80/default.rss" : "http://localhost:80/proxy/"+subUrl,
             success: function (feed) {
-                if (!feed)
+				var i, splitEstimate, estimateString;
+                if (!feed) {
                     return;
+				}
 
-                for (var i = 0; i < feed.items.length; i++) {
-                    var splitEstimate = /.*Estimate(.*)/.exec(feed.items[i].description);
-                    var estimateString = "0";
-                    if (splitEstimate && splitEstimate.length > 0)
+                for (i = 0; i < feed.items.length; i++) {
+                    splitEstimate = /.*Estimate(.*)/.exec(feed.items[i].description);
+                    estimateString = "0";
+                    if (splitEstimate && splitEstimate.length > 0) {
                         estimateString = splitEstimate[1].replace(/[: \na-zA-Z]/g, '');
+					}
 
                     data.push({
                         name: feed.items[i].title,
